4873372731957cfab9edcc99f9c607f160cfd1ce
sfallou <sfallou2010&hotmail.fr>
2017-03-27 20:18:40 +0200

    Avancement à 98%

--- Circhain/src/pages/carte-details/carte-details.ts
+++ Circhain/src/pages/carte-details/carte-details.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
+import { NavController, NavParams, AlertController } from 'ionic-angular';
 import {CirchainService} from '../mesServices/circhainServices';
 import {LoginService} from '../mesServices/loginService';
 
@@ -16,11 +16,13 @@ export class CarteDetailsPage {
 
   id_carte : any;
   date_creation : any;
+  proprietaire_final: any;
   etat_carte : any;
+  certification : any;
   proprietaire_actu : any;
   historique : any;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, private detailsCarte: CirchainService, private loginService: LoginService) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, public alertCtrl: AlertController, private detailsCarte: CirchainService, private loginService: LoginService) {
     // If we navigated to this page, we will have an item available as a nav param
     this.selectedItem = navParams.get('item');
     this.acteur = JSON.parse(this.loginService.getInfosActeur());
@@ -28,8 +30,10 @@ export class CarteDetailsPage {
 				data => {
 					this.status = data.status;
 					//this.infosCarte = data.data;
-         //console.log(this.infosCarte);
+         console.log(data.data);
          this.id_carte = data.data.id_carte;
+         this.proprietaire_final = data.data.proprietaire_final;
+         this.certification = data.data.certification;
          this.proprietaire_actu = data.data.proprietaire_actuel;
          this.date_creation = data.data.date_creation;
          this.etat_carte = data.data.etat_carte;
@@ -41,5 +45,15 @@ export class CarteDetailsPage {
 					console.log(err);
 				}
 			);//
+  }
+
+  infoCertification() {
+   let alert = this.alertCtrl.create({
+     title: "Information sur les certifications",
+     subTitle: "Certifiée A : la carte peut être intégrée dans un circuit d'avion,Certifié B : la carte peut être intégrée dans un circuit de train ",
+     buttons: ['OK']
+   });
+   alert.present();
+             
   }
 }
