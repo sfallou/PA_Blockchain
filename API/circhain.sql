6d0f1cb1f378ef8419c3c9590616d29626a6af84
sfallou <sfallou2010@hotmail.fr>
2017-06-18 15:31:59 +0200

    Update circhain.sql

--- API/circhain.sql
+++ API/circhain.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Client :  localhost:8889
--- Généré le :  Lun 13 Mars 2017 à 20:13
+-- Généré le :  Dim 26 Mars 2017 à 01:03
 -- Version du serveur :  5.6.35
 -- Version de PHP :  5.6.30
 
@@ -29,6 +29,7 @@ CREATE TABLE `Acteurs` (
   `adresse_acteur` varchar(255) DEFAULT NULL,
   `tel_acteur` varchar(255) DEFAULT NULL,
   `profil` varchar(50) DEFAULT NULL,
+  `homologation` varchar(255) DEFAULT NULL,
   `timestamp` varchar(70) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL,
   `mdp` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
@@ -37,12 +38,15 @@ CREATE TABLE `Acteurs` (
 -- Contenu de la table `Acteurs`
 --
 
-INSERT INTO `Acteurs` (`id`, `id_acteur`, `nom_acteur`, `prenom_acteur`, `email_acteur`, `adresse_acteur`, `tel_acteur`, `profil`, `timestamp`, `mdp`) VALUES
-(1, 'CCIIRRLLYY', 'Françon', 'Maxime', 'maxime.francon@cirly.com', 'Brignais', '0476890045', 'administrateur', '1489432091', '6d1b74393fb219ad5380838006a9a1d46d256e75'),
-(2, 'AALLSSTTOOMM', 'Adam', 'Mark', 'mark.adam@alstom.com', 'Paris', '0406890045', 'client', '1489432091', '629495600e3c30ab9dd16dd13e4c31bba1e7da0c'),
-(3, 'AARREEVVAA', 'Frank', 'Taylor', 'frank.taylor@areva.com', 'Valence', '476888045', 'client', '1489432091', '9e3a3d3fe136affa24bfe3137c044e067262f09e'),
-(4, 'FFRRIILLNN', 'Pamerlo', 'Jean', 'jean.pamerlo@cable1.com', 'Turin', '0376888045', 'cableur', '1489432091', '8a1eff894f01eda33a3152c3393874176de5fe91'),
-(5, 'TEST', 'Test', 'Test', 'test.test@test.com', 'Paris', '0006890045', 'client', '1489432091', 'a94a8fe5ccb19ba61c4c0873d391e987982fbbd3');
+INSERT INTO `Acteurs` (`id`, `id_acteur`, `nom_acteur`, `prenom_acteur`, `email_acteur`, `adresse_acteur`, `tel_acteur`, `profil`, `homologation`, `timestamp`, `mdp`) VALUES
+(1, 'CIRLY', 'Frachon', 'Maxime', 'frachon@cirly.com', 'Brignais', '0472715955', 'administrateur', 'A,B,C,D', '1490484667', '6d1b74393fb219ad5380838006a9a1d46d256e75'),
+(2, 'ALSTOM', 'Adam', 'Mark', 'mark.adam@alstom.com', 'Paris', '0406890045', 'client', '', '1490484667', '629495600e3c30ab9dd16dd13e4c31bba1e7da0c'),
+(3, 'AREVA', 'Frank', 'Taylor', 'frank.taylor@areva.com', 'Valence', '476888045', 'client', '', '1490484667', '9e3a3d3fe136affa24bfe3137c044e067262f09e'),
+(4, 'CTS', 'Pamerlo', 'Jean', 'jean.pamerlo@cts.com', 'CTS industries', '0376888045', 'cableur', 'A,B,C,D', '1490484667', '70460c6ba8049d797b3b0f2922dcca0387f2d258'),
+(5, 'BSE', 'Loic', 'Ribalt', 'loic.ribalt@bse.com', 'Lille', '0376888045', 'cableur', 'A,B,C', '1490484667', 'cd7705dd26a434f0822911169275f3a001728a32'),
+(6, 'EOLANE', 'Moreau', 'Hugues', 'hugues.moreau@eolane.com', 'Ecully', '0376888045', 'cableur', 'A,B', '1490484667', '62e4e012916d77de2c2c47276437e51389c8e82c'),
+(7, 'ASTEEL', 'Hachim', 'khalid', 'khalid.hachim@asteel.com', 'Asteel Flash', '0376888045', 'cableur', 'A', '1490484667', '7a538e91ab9a610357164efb65eee698bde6e7c9'),
+(8, 'CABLEUR', 'NS', 'Paola', 'paola.ns@cableur.com', 'Paris', '0376888045', 'cableur', '', '1490484720', 'ccdb090a250b8462fb22b2a6714ad1f892607c10');
 
 -- --------------------------------------------------------
 
@@ -65,15 +69,16 @@ CREATE TABLE `Acteurs_Cartes` (
 --
 
 INSERT INTO `Acteurs_Cartes` (`id`, `id_proprietaire_actu`, `id_proprietaire_prec`, `id_carte`, `date_emis`, `date_recu`, `timestamp`) VALUES
-(1, 'AARREEVVAA', 'CCIIRRLLYY', 'carte1', '2017-03-13 20:12:00', NULL, '1489432320'),
-(2, 'AARREEVVAA', 'CCIIRRLLYY', 'carte2', '2017-03-13 20:12:00', NULL, '1489432320'),
-(3, 'AALLSSTTOOMM', 'CCIIRRLLYY', 'carte3', '2017-03-13 20:12:00', NULL, '1489432320'),
-(4, 'AALLSSTTOOMM', 'CCIIRRLLYY', 'carte4', '2017-03-13 20:12:00', NULL, '1489432320'),
-(5, 'TEST', 'CCIIRRLLYY', 'carte5', '2017-03-13 20:12:00', NULL, '1489432320'),
-(6, 'TEST', 'CCIIRRLLYY', 'carte6', '2017-03-13 20:12:00', NULL, '1489432320'),
-(7, 'AARREEVVAA', 'CCIIRRLLYY', 'carte7', '2017-03-13 20:12:00', NULL, '1489432320'),
-(8, 'FFRRIILLNN', 'CCIIRRLLYY', 'carte8', '2017-03-13 20:12:00', NULL, '1489432320'),
-(9, 'FFRRIILLNN', 'CCIIRRLLYY', 'carte9', '2017-03-13 20:12:00', NULL, '1489432320');
+(1, 'CIRLY', 'CIRLY', 'carte1', '2017-03-26 00:44:59', '2017-03-26 00:44:59', '1490485499'),
+(2, 'CIRLY', 'CIRLY', 'carte2', '2017-03-26 00:44:59', '2017-03-26 00:44:59', '1490485499'),
+(3, 'CIRLY', 'CIRLY', 'carte3', '2017-03-26 00:44:59', '2017-03-26 00:44:59', '1490485499'),
+(4, 'CIRLY', 'CIRLY', 'carte4', '2017-03-26 00:44:59', '2017-03-26 00:44:59', '1490485499'),
+(5, 'CIRLY', 'CIRLY', 'carte5', '2017-03-26 00:44:59', '2017-03-26 00:44:59', '1490485499'),
+(6, 'CIRLY', 'CIRLY', 'carte6', '2017-03-26 00:44:59', '2017-03-26 00:44:59', '1490485499'),
+(7, 'CIRLY', 'CIRLY', 'carte7', '2017-03-26 00:44:59', '2017-03-26 00:44:59', '1490485499'),
+(8, 'CIRLY', 'CIRLY', 'carte8', '2017-03-26 00:44:59', '2017-03-26 00:44:59', '1490485499'),
+(9, 'CIRLY', 'CIRLY', 'carte9', '2017-03-26 00:44:59', '2017-03-26 00:44:59', '1490485499'),
+(10, 'CIRLY', 'CIRLY', 'carte10', '2017-03-26 00:44:59', '2017-03-26 00:44:59', '1490485499');
 
 -- --------------------------------------------------------
 
@@ -85,8 +90,10 @@ CREATE TABLE `Cartes` (
   `id` int(11) NOT NULL,
   `id_carte` varchar(255) DEFAULT NULL,
   `proprietaire_actu` varchar(255) DEFAULT NULL,
+  `proprietaire_final` varchar(50) NOT NULL,
   `etat_carte` varchar(255) DEFAULT NULL,
   `date_creation` datetime DEFAULT NULL,
+  `certification` varchar(255) DEFAULT NULL,
   `timestamp` varchar(75) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
@@ -94,17 +101,17 @@ CREATE TABLE `Cartes` (
 -- Contenu de la table `Cartes`
 --
 
-INSERT INTO `Cartes` (`id`, `id_carte`, `proprietaire_actu`, `etat_carte`, `date_creation`, `timestamp`) VALUES
-(1, 'carte1', 'AARREEVVAA', 'en transit', '2017-03-13 20:11:03', '1489432320'),
-(2, 'carte2', 'AARREEVVAA', 'en transit', '2017-03-13 20:11:03', '1489432320'),
-(3, 'carte3', 'AALLSSTTOOMM', 'en transit', '2017-03-13 20:11:03', '1489432320'),
-(4, 'carte4', 'AALLSSTTOOMM', 'en transit', '2017-03-13 20:11:03', '1489432320'),
-(5, 'carte5', 'TEST', 'en transit', '2017-03-13 20:11:03', '1489432320'),
-(6, 'carte6', 'TEST', 'en transit', '2017-03-13 20:11:03', '1489432320'),
-(7, 'carte7', 'AARREEVVAA', 'en transit', '2017-03-13 20:11:03', '1489432320'),
-(8, 'carte8', 'FFRRIILLNN', 'en transit', '2017-03-13 20:11:03', '1489432320'),
-(9, 'carte9', 'FFRRIILLNN', 'en transit', '2017-03-13 20:11:03', '1489432320'),
-(10, 'carte10', 'CCIIRRLLYY', 'reçue', '2017-03-13 20:11:03', '1489432263');
+INSERT INTO `Cartes` (`id`, `id_carte`, `proprietaire_actu`, `proprietaire_final`, `etat_carte`, `date_creation`, `certification`, `timestamp`) VALUES
+(1, 'carte1', 'CIRLY', 'AREVA', 'reçue', '2017-03-26 00:44:59', 'A,B,C,D', '1490485499'),
+(2, 'carte2', 'CIRLY', 'AREVA', 'reçue', '2017-03-26 00:44:59', 'A,B,C,D', '1490485499'),
+(3, 'carte3', 'CIRLY', 'ALSTOM', 'reçue', '2017-03-26 00:44:59', 'A,B,C,D', '1490485499'),
+(4, 'carte4', 'CIRLY', 'AREVA', 'reçue', '2017-03-26 00:44:59', 'A,B,C,D', '1490485499'),
+(5, 'carte5', 'CIRLY', 'ALSTOM', 'reçue', '2017-03-26 00:44:59', 'A,B,C,D', '1490485499'),
+(6, 'carte6', 'CIRLY', 'AREVA', 'reçue', '2017-03-26 00:44:59', 'A,B,C,D', '1490485499'),
+(7, 'carte7', 'CIRLY', 'ALSTOM', 'reçue', '2017-03-26 00:44:59', 'A,B,C,D', '1490485499'),
+(8, 'carte8', 'CIRLY', 'ALSTOM', 'reçue', '2017-03-26 00:44:59', 'A,B,C,D', '1490485499'),
+(9, 'carte9', 'CIRLY', 'AREVA', 'reçue', '2017-03-26 00:44:59', 'A,B,C,D', '1490485499'),
+(10, 'carte10', 'CIRLY', 'ALSTOM', 'reçue', '2017-03-26 00:44:59', 'A,B,C,D', '1490485499');
 
 -- --------------------------------------------------------
 
@@ -122,21 +129,6 @@ CREATE TABLE `Notifications` (
   `date_envoi` datetime DEFAULT NULL,
   `timestamp` varchar(75) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=COMPACT;
-
---
--- Contenu de la table `Notifications`
---
-
-INSERT INTO `Notifications` (`id`, `id_acteur`, `id_destinataire`, `id_carte_concernee`, `type_notification`, `etat_notification`, `date_envoi`, `timestamp`) VALUES
-(1, 'CCIIRRLLYY', 'AARREEVVAA', 'carte1', 'émise', 0, '2017-03-13 20:12:00', '1489432320'),
-(2, 'CCIIRRLLYY', 'AARREEVVAA', 'carte2', 'émise', 0, '2017-03-13 20:12:00', '1489432320'),
-(3, 'CCIIRRLLYY', 'AALLSSTTOOMM', 'carte3', 'émise', 0, '2017-03-13 20:12:00', '1489432320'),
-(4, 'CCIIRRLLYY', 'AALLSSTTOOMM', 'carte4', 'émise', 0, '2017-03-13 20:12:00', '1489432320'),
-(5, 'CCIIRRLLYY', 'TEST', 'carte5', 'émise', 0, '2017-03-13 20:12:00', '1489432320'),
-(6, 'CCIIRRLLYY', 'TEST', 'carte6', 'émise', 0, '2017-03-13 20:12:00', '1489432320'),
-(7, 'CCIIRRLLYY', 'AARREEVVAA', 'carte7', 'émise', 0, '2017-03-13 20:12:00', '1489432320'),
-(8, 'CCIIRRLLYY', 'FFRRIILLNN', 'carte8', 'émise', 0, '2017-03-13 20:12:00', '1489432320'),
-(9, 'CCIIRRLLYY', 'FFRRIILLNN', 'carte9', 'émise', 0, '2017-03-13 20:12:00', '1489432320');
 
 --
 -- Index pour les tables exportées
@@ -176,12 +168,12 @@ ALTER TABLE `Notifications`
 -- AUTO_INCREMENT pour la table `Acteurs`
 --
 ALTER TABLE `Acteurs`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 --
 -- AUTO_INCREMENT pour la table `Acteurs_Cartes`
 --
 ALTER TABLE `Acteurs_Cartes`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 --
 -- AUTO_INCREMENT pour la table `Cartes`
 --
@@ -191,4 +183,4 @@ ALTER TABLE `Cartes`
 -- AUTO_INCREMENT pour la table `Notifications`
 --
 ALTER TABLE `Notifications`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
